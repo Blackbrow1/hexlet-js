06dd5661f9f6dc75ce6fa3b6c99a8f94921b78ab
Aleksandr Chernobrov <alesandro83@mail.ru>
2022-05-12 21:47:01 +0300

    Добавил файл prettier

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 import _ from 'lodash';
 
-console.log(_.last(['one', 'two']));
+let person =                     {
+    name: "Yoda",
+                  designation: 'Jedi Master '
+                  };
+  
+  
+                function trainJedi (jediWarrion) {
+  if (jediWarrion.name === 'Yoda') {
+    console.log('No need! already trained');
+  }
+  console.log(`Training ${jediWarrion.name} complete`)
+                }
+  
+  trainJedi(person)
+                trainJedi({ name: 'Adeel',
+                designation: 'padawan' 
+    });
+
+    let a;
